c8ae937dc5b16f0e1568bfcebcbe180de4fd4f87
Thenis <d.stoilov96@gmail.com>
2017-04-29 11:51:13 +0300

    Added list movies function. Lists movies from input

--- js/main.js
+++ js/main.js
@@ -7,11 +7,41 @@ let movieList = (function () {
             movies.set(counter, item);
             counter++;
             },
-        deleteMovie: (item) => movies.delete(item),
+        deleteMovie: (key) => movies.delete(key),
         toString:() => console.log(movies.values()),
-        mapKeys: () => console.log(movies.keys())
+        mapKeys: () => console.log(movies.keys()),
+        getMovies: () => {
+            return movies;
+        }
     }
 })();
+
+function listMovies() {
+    let tbody = document.getElementById("movie-list");
+
+    tbody.innerHTML = "";// clear table child elements
+
+
+    movieList.getMovies().forEach(function (value, key) {
+        let tr = document.createElement("tr");
+        let tdMovie = document.createElement("td");
+        let tdRating = document.createElement("td");
+
+        tdMovie.innerText = value.movieName;
+
+        tr.appendChild(tdMovie);
+        tbody.appendChild(tr);
+
+
+        console.log(`${key} = ${value.movieName}`)
+
+
+    });
+
+
+
+
+}
 
 function addMovie() {
     let movieName = document.getElementById("movie-input").value;
@@ -19,8 +49,9 @@ function addMovie() {
     movieList.addMovie({movieName: movieName, date: Date.now(), watched: false, rating: "Not rated"});
     //console.log(getDate);
     //console.log(movieName);
-    movieList.toString();
-    movieList.mapKeys();
+   /* movieList.toString();
+    movieList.mapKeys();*/
+    listMovies();
 }
 
 /*movieList.addMovie({name: "GOT"});
